45673052039042b27323e94f32705cfe42933467
Tenzin Jorden <tenden96@gmail.com>
2021-12-13 15:16:26 -0800

    commit tree d7994f82a99474d0a309fe751355a992a9c69bfa

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+var num1  = 62;
+var num2 = 1;
+
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 24
+var num4 = 6
+
+var mod = num3 / num4;
+
+var max = Math.max(1, 2, 3, 4, 20);
